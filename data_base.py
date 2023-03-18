cd2168962941275287af7a4146d45e6c94916e05
Maxim <119751147+Xiinap@users.noreply.github.com>
2023-03-18 18:16:48 +0400

    Create data_base.py

---
+++ data_base.py
@@ -0,0 +1,29 @@
+'''
+name     score
+
+name1    400
+name2    100
+name3    500
+'''
+import sqlite3
+
+db = sqlite3.connect("2048.sqlite")
+cursor = db.cursor()
+
+cursor.execute("""
+create table if not exists RECORDS(
+    name text,
+    score integer
+)""")
+
+def get_best():
+    cursor.execute("""
+        SELECT ROWID, name, score from RECORDS
+        GROUP by name
+        ORDER by score DESC
+        limit 3
+    """)
+    return cursor.fetchall()
+
+result = get_best()
+print(result)
