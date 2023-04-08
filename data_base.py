4d1dd662b288d4f58ea1ee58e320f5722eee5eb2
Maxim <119751147+Xiinap@users.noreply.github.com>
2023-04-08 17:53:25 +0400

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -8,13 +8,14 @@ name3    500
 import sqlite3
 
 db = sqlite3.connect("2048.sqlite")
-cursor = db.cursor()
 
+cursor = db.cursor()
 cursor.execute("""
 create table if not exists RECORDS(
     name text,
     score integer
-)""")
+    )""")
+
 
 def insert_result(name, score):
     cursor.execute("""
@@ -22,14 +23,17 @@ def insert_result(name, score):
     """, (name, score))
     db.commit()
 
+
 def get_best():
     cursor.execute("""
-        SELECT ROWID, name, score from RECORDS
+        SELECT name, max(score) score from RECORDS
         GROUP by name
         ORDER by score DESC
         limit 3
     """)
+
     return cursor.fetchall()
+
 
 result = get_best()
 #print(result)
