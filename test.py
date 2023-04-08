4cbe9954c949cd496fd994f7794a1e080b3276e6
Maxim <119751147+Xiinap@users.noreply.github.com>
2023-04-08 17:54:39 +0400

    Update test.py

--- test.py
+++ test.py
@@ -1,42 +1,69 @@
-#test file
-#tester job =)
-'''
-importing
-'''
 import unittest
-from main import *
+from logic import *
 
-class Test2048(unittest.TestCase):
 
-    def test_1(self): #test 1
+class Test_2048(unittest.TestCase):
+
+    def test_1(self):
         self.assertEqual(get_number_from_index(1, 2), 7)
 
-    def test_2(self): #test 2
+    def test_2(self):
         self.assertEqual(get_number_from_index(3, 3), 16)
 
-    def test_3(self): #test 3
-        test_mas = [1, 2, 3, 4, 5, 6, 7, 8, 10, 11, 12, 13, 14, 16]
-        mas = [[0, 0, 0, 0],
-               [0, 0, 0, 0],
-               [1, 0, 0, 0],
-               [0, 0, 3, 0]]
-        self.assertEqual(get_empty_list(mas), test_mas)
+    def test_3(self):
+        a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        mas = [
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+        self.assertEqual(get_empty_list(mas), a)
 
-    def test_4(self): #test 4
-        test_mas = []
-        mas = [[1, 1, 1, 1],
-               [1, 1, 1, 1],
-               [1, 1, 1, 1],
-               [1, 1, 1, 1]]
-        self.assertEqual(get_empty_list(mas), test_mas)
+    def test_4(self):
+        a = []
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(get_empty_list(mas), a)
 
-    def test_5(self): #test 5
-        test_mas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
-        mas = [[0, 0, 0, 0],
-               [0, 0, 0, 0],
-               [0, 0, 0, 0],
-               [0, 0, 0, 0]]
-        self.assertEqual(get_empty_list(mas), test_mas)
+    def test_5(self):
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(is_zero_in_mas(mas), False)
 
-    def test_6(self): #test 6
-        self.assertEqual(get_index_from_number(16), (3, 0))
+    def test_6(self):
+        mas = [
+            [1, 1, 1, 1],
+            [1, 1, 0, 1],
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+        ]
+        self.assertEqual(is_zero_in_mas(mas), True)
+
+    def test_move_left(self):
+        mas = [
+            [2, 4, 4, 2],
+            [4, 0, 0, 2],
+            [0, 0, 0, 0],
+            [8, 8, 4, 4],
+        ]
+        rez = [
+            [2, 8, 2, 0],
+            [4, 2, 0, 0],
+            [0, 0, 0, 0],
+            [16, 8, 0, 0],
+        ]
+
+
+        self.assertEqual(move_left(mas), (rez, 32))
+
+if __name__ == 'main':
+    unittest.main()
